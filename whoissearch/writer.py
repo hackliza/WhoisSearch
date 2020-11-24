1b2822fa964dd2f0c2816db3125b05d7ade427ba
Guzman Cernadas Perez <guzman.cernadas@gmail.com>
2020-11-24 18:51:49 +0100

    Now shows the output file

--- whoissearch/writer.py
+++ whoissearch/writer.py
@@ -7,18 +7,20 @@ from whoissearch.logger import Logger
 
 class Writer:
     def write_csv(self, data, output_directory):
-        Logger().info("Writing CSV")
+        output_file = os.path.join(output_directory, "result.csv")
+        Logger().info("Writing CSV in {output_file}".format(output_file=output_file))
         os.makedirs(output_directory, exist_ok=True)
         header = ["inetnum", "netname", "descr", "country", "admin_c", "tech_c", "remarks", "notify", "mnt_by",
                   "changed", "status", "mnt_lower", "created", "last_modified", "source", "matched_word"]
         data_to_write = [list(net) for net in data]
-        with open(os.path.join(output_directory, "result.csv"), "w", encoding="utf8", newline='') as csv_file:
+        with open(output_file, "w", encoding="utf8", newline='') as csv_file:
             writer = csv.writer(csv_file, delimiter=";", quotechar='"')
             writer.writerow(header)
             writer.writerows(data_to_write)
 
     def write_json(self, data, output_directory):
-        Logger().info("Writing JSON")
+        output_file = os.path.join(output_directory, "result.json")
+        Logger().info("Writing JSON in {output_file}".format(output_file=output_file))
         results = [obj.to_dict() for obj in data]
-        with open(os.path.join(output_directory, "result.json"), "w", encoding="utf8") as file:
+        with open(output_file, "w", encoding="utf8") as file:
             json.dump({"results": results}, file, indent=4)
