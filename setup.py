27053a02c9b8037cf11a7f756179f87d2463abad
Caralludo <72373081+Caralludo@users.noreply.github.com>
2024-03-23 11:46:44 +0100

    Updated version to 0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="whoissearch",
-    version="0.1.0",
+    version="0.1.1",
     author="Guzman Cernadas Perez",
     author_email="guzman.cernadas@protonmail.com",
     description="Get network blocks from whois from a list of words",
